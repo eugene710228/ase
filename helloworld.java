fa55da85dd67f2583f29aadd8424d0a2e6e6f683
eugene710228 <eugene710228@users.noreply.github.com>
2015-10-04 21:55:28 +0800

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+package com.fcu.software;
+
+public class sc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String aa="Hello World";
+		System.out.print(aa);
+	}
+
+}
