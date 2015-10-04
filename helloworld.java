4a0def7237e9307932f5c304d3942c4134ea21c6
Eugene Chen <m0415354@fcu.edu.tw>
2015-10-04 22:11:18 +0800

    push to github first

--- helloworld.java
+++ helloworld.java
@@ -4,8 +4,9 @@ public class sc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String aa="Hello World";
+		String aa="download_GitHub";
 		System.out.print(aa);
+		
 	}
 
 }
