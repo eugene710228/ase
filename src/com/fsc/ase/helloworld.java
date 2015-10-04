56bf5e8b97e58e7e4370b2b7d030edbbbed45b42
Eugene Chen <m0415354@fcu.edu.tw>
2015-10-04 22:20:17 +0800

    create src

---
+++ src/com/fsc/ase/helloworld.java
@@ -0,0 +1,13 @@
+package com.fsc.ase;
+
+public class helloworld {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String aa="download_GitHub";
+		System.out.print(aa);
+		
+	}
+
+}
