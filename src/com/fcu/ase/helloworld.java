e56ef24fd6a1d3613739d8b665cdb6185b4136e3
joejack212 <joejack212@gmail.com>
2015-10-15 20:50:00 +0800

    joejack212

--- src/com/fcu/ase/helloworld.java
+++ src/com/fcu/ase/helloworld.java
@@ -6,6 +6,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String bb="helloworld_master";
+		String cc="helloworld_m0413234";
 		System.out.print(bb);
 		System.out.println("Welcome to course of software engineering !!!");
 		String id="allpower";
