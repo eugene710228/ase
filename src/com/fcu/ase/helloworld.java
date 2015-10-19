87e4162ef0cb738ffb2ec8ef4d26114260a9b9d8
Administrator <Administrator@speed.mshome.net>
2015-10-19 12:36:27 +0800

    add some word

--- src/com/fcu/ase/helloworld.java
+++ src/com/fcu/ase/helloworld.java
@@ -6,12 +6,12 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String bb="helloworld_master";
-		String cc="helloworld_m0413234";
+		String cc="helloworld_m0413779";
 		System.out.print(bb);
 		System.out.println("Welcome to course of software engineering !!!");
 		String id="allpower123";
-		String murmur = "time to sleep!";
-		String murmur = "I see you!";
+		String murmur = "time to sleep!,go to sleep now";
+		String murmur = "I see you!I see you too";
 		
 	}
 
